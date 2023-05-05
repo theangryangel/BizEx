afa25cfe5c81a06c34bfc3cba45eb608c7ee4dc5
Niklas <niklas@odd.software>
2023-05-05 04:01:38 +0300

    Update deps and elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BizEx.MixProject do
     [
       app: :bizex,
       version: "0.2.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs()
@@ -25,10 +25,10 @@ defmodule BizEx.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
 
-      {:timex, "~> 3.0"},
-      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.16", only: [:dev], runtime: false}
+      {:timex, "~> 3.7"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.27", only: [:dev], runtime: false}
     ]
   end
 
